09297d95ec80f04b9d478500ea121d2c07e605f9
Bruce Ide <FlyingRhenquest@gmail.com>
2013-07-23 21:13:53 -0600

    Added interpolation and filtering for min and max altitudes. Also added a stripped-down version of skydiving.gpx with just 1 jump worth of data, which is a bit easier to work with until I'm ready to try processing multiple jumps.

--- gpx2kml.cpp
+++ gpx2kml.cpp
@@ -41,14 +41,24 @@
 
 // gpx factory notifies with the posix time as a double and a lat_long
 // coordinate (But I want my coordinates in ECEF for later analysis)
-typedef std::pair<double, fr::coordinates::ecef> coordinate_pair;
+typedef std::pair<double, fr::coordinates::ecef_vel> coordinate_pair;
 typedef std::vector<coordinate_pair> coordinate_vector;
 
 // Populate_coordinates is the listener for the gpx factory
-void populate_coordinates(coordinate_vector *coordinates, double at_time, fr::coordinates::lat_long point)
+void populate_coordinates(coordinate_vector *coordinates, double at_time, fr::coordinates::lat_long point, fr::coordinates::ecef_vel *last_point, double *last_time)
 {
   fr::coordinates::ecef point_ecef = fr::coordinates::converter<fr::coordinates::ecef>()(point);
-  coordinates->push_back(std::make_pair(at_time, point_ecef));
+  // But I also want the deltas for interpolation
+  double dx = 0.0;
+  double dy = 0.0;
+  double dz = 0.0;
+  if (*last_time > 0.0) {
+    dx = point_ecef.get_x() - last_point->get_x();
+    dy = point_ecef.get_y() - last_point->get_y();
+    dz = point_ecef.get_z() - last_point->get_z();
+  }
+  fr::coordinates::ecef_vel vel(point_ecef.get_x(), point_ecef.get_y(), point_ecef.get_z(), dx, dy, dz);
+  coordinates->push_back(std::make_pair(at_time, vel));
 }
 
 // Print KML document to selected stream
@@ -60,13 +70,35 @@ void output_kml(std::ostream &out, cppxml::xml_node::pointer kml_document)
 
 // Add a coordinate for a point. I include last point in case you
 // want to compute deltas or something
-void add_coordinate(cppxml::kml_linestring::pointer to_this, const coordinate_pair &coordinates, double last_time, fr::coordinates::ecef last_point)
+void add_coordinate(cppxml::kml_linestring::pointer to_this, const coordinate_pair &coordinates, double last_time, fr::coordinates::ecef_vel last_point, double min, double max, double interpolate_step)
 {
   timeval tv;
   
   // Dropping factions for the time being. 
   tv.tv_usec = 0;
   tv.tv_sec = (long) coordinates.first;
+  fr::coordinates::lat_long point = fr::coordinates::converter<fr::coordinates::lat_long>()(coordinates.second);
+
+  if (point.get_alt() < min) {
+    return;
+  }
+
+  if (max > 0.0 && point.get_alt() > max) {
+    return;
+  }
+
+  // Interpolate sub-second intervals
+  if (last_time > 0.0 && interpolate_step > 0.0) {
+    fr::coordinates::tod_eci_vel first_point = fr::coordinates::converter<fr::coordinates::tod_eci_vel>()(last_point, last_time);
+    fr::coordinates::tod_eci_vel second_point = fr::coordinates::converter<fr::coordinates::tod_eci_vel>()(coordinates.second, coordinates.first);
+    for (double i = last_time + interpolate_step ; i < coordinates.first - .05 ; i += interpolate_step) {
+      fr::coordinates::tod_eci_vel mid_point = first_point.interpolate(last_time, second_point, coordinates.first, i);
+      fr::coordinates::ecef_vel mid_point_ecef = fr::coordinates::converter<fr::coordinates::ecef_vel>()(mid_point, i);
+      coordinate_pair inter_point = std::make_pair(i,mid_point_ecef);
+      // So... not...
+      add_coordinate(to_this, inter_point, 0.0, last_point, min, max, 0.0);
+    }
+  }
 
   to_this->add(coordinates.second);
   
@@ -78,8 +110,21 @@ int main(int argc, char *argv[])
   // Affix timezone to GMT (Which is what the GPX file format is in)
   fr::time::timezone_manager timezone_manager("GMT");
   boost::program_options::options_description desc("Usage");
+  double min_altitude = 0.0; // filter points below this
+  double max_altitude = 0.0; // If this is grater than 0, filter points aboove this
+  double interpolate_step = 0.1; // 0.0 for none
+
   desc.add_options()
     ("help,h", "Print help")
+    ("min",
+     boost::program_options::value<double>(&min_altitude)->default_value(0.0),
+     "Filter points below this altitude")
+    ("max",
+     boost::program_options::value<double>(&max_altitude)->default_value(0.0),
+     "If this is more than 0, filter points above this altitude")
+    ("step",
+     boost::program_options::value<double>(&interpolate_step)->default_value(0.1),
+     "Interpolation step (0.0 for none)")
     ("input_filename,i",
      boost::program_options::value<std::string>(&input_filename)->required(),
      "GPX file from mytracks");
@@ -115,26 +160,27 @@ int main(int argc, char *argv[])
 
   coordinate_vector coordinates;
   fr::data::gpx_factory factory(input_filename);
-  factory.available.connect(boost::bind(&populate_coordinates, &coordinates, _1, _2));
+  fr::coordinates::ecef_vel last_point(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+  double last_time = 0.0;
+
+  factory.available.connect(boost::bind(&populate_coordinates, &coordinates, _1, _2, &last_point, &last_time));
   factory.process();
   // We should now have a vector of coordinates all ready to grind up into
   // a yummy coordinate sausage!
 
   // Lets just get it out as quickly as possible right now
-  double last_time = 0.0;
-  fr::coordinates::ecef last_point(0.0, 0.0, 0.0);
 
   cppxml::kml_document::pointer document = std::make_shared<cppxml::kml_document>("gpx2kml output");
   cppxml::kml_folder::pointer folder = std::make_shared<cppxml::kml_folder>("Coordinates");
   cppxml::kml_placemark::pointer placemark = std::make_shared<cppxml::kml_placemark>();
-  cppxml::kml_linestring::pointer linestring = std::make_shared<cppxml::kml_linestring>();
+  cppxml::kml_linestring::pointer linestring = std::make_shared<cppxml::kml_linestring>(cppxml::absolute, false);
 
   folder->add_child(placemark);
   placemark->add_child(linestring);
   document->add_child(folder);
 
   for (const coordinate_pair &pair : coordinates) {
-    add_coordinate(linestring, pair, last_time, last_point);
+    add_coordinate(linestring, pair, last_time, last_point, min_altitude, max_altitude, interpolate_step);
     last_time = pair.first;
     last_point = pair.second;
   }
